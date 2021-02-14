171679f4cf0e4da1651391125f267e7ad5216d31
Shane <shanemailer@gmail.com>
2021-02-14 17:28:02 -0600

    chore(update): targetSdkVersion, dependencies, android X, add privacy link

--- app/src/main/java/com/shane_taylor/Graphy/MenuPageActivity.java
+++ app/src/main/java/com/shane_taylor/Graphy/MenuPageActivity.java
@@ -3,16 +3,31 @@ package com.shane_taylor.Graphy;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
+import android.widget.TextView;
+
+import static android.content.ContentValues.TAG;
+import static com.shane_taylor.Graphy.R.id.privacyLink;
 
 public class MenuPageActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_menu_page);
+
+        // 1) How to replace link by text like "Click Here to visit Google" and
+        // the text is linked with the website url ?
+        TextView link = (TextView) findViewById(privacyLink);
+        String linkText = "<a href='https://www.shanetaylor.dev/#/graphy-privacy-terms'>Privacy policy here.</a>";
+        link.setText(Html.fromHtml(linkText));
+        link.setMovementMethod(LinkMovementMethod.getInstance());
     }
 
     public void onClickTruth(View view) {
